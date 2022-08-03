627de8f1c890d5d1afff7b305b426b0425b339c9
yogeshreeturkar111 <110226741+yogeshreeturkar111@users.noreply.github.com>
2022-08-03 23:07:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Perfect
+{
+    public static void Main()
+    {
+        int n, i, mn, mx, sum;
+
+        Console.Write("\n\n");
+        Console.Write("Find perfect numbers within a given number of range:\n");
+        Console.Write("------------------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("Input the starting range or number : ");
+        mn = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the ending range of number : ");
+        mx = Convert.ToInt32(Console.ReadLine());
+        Console.Write("The Perfect numbers within the given range : ");
+        for (n = mn; n <= mx; n++)
+        {
+            i = 1;
+            sum = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                    sum = sum + i;
+                i++;
+            }
+            if (sum == n)
+                Console.Write("{0} ", n);
+        }
+        Console.Write("\n");
+    }
+}
